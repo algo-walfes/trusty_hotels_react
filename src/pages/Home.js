e9831a6bf1cbef84c4e12940a3e52c0b49d98dbc
Mohammadnim123 <mm.nemrawi96@gmail.com>
2020-10-15 23:19:05 +0300

    comp and pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,13 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Services from "../components/Services";
-
+import FeturedRooms from "../components/FeaturedRooms";
 const Home = () => {
     return (
         <>
         <Hero>
         <Banner
-          title="luxurious rooms"
+          title="trusty hotels"
           subtitle="deluxe rooms starting at $299"
         >
           <Link to="/rooms" className="btn-primary">
@@ -18,6 +18,8 @@ const Home = () => {
         </Banner>
         </Hero>
         <Services />
+        <FeturedRooms />
+
         </>
     )
 }
