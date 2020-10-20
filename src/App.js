82845087c61b69fb79da34dd9d720056424ffcea
3madov-77 <emadzxy7@gmail.com>
2020-10-20 23:59:28 +0300

    add auth0 service

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Error from "./pages/Error";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Favorite from "./pages/Favorite";
+
+// import LoginButton from "./components/LoginButton";
 
 
 function App() {
@@ -18,6 +21,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
         <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/favorite" component={Favorite} />
         <Route component={Error} />
       </Switch>
       <Footer/>
